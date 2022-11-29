7a48868ecdc26b0cbfbae6eb10b5b26f4bbc4490
Hannah Maratea <maratea101@gmail.com>
2022-11-29 11:59:13 -0500

    commit tree ebfeb72895fd5aa369dd516575a88d6d96883529

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, personWeAreLookingFor) {
+    return drivers.filter((el) => el.toLowerCase().includes(personWeAreLookingFor.toLowerCase()));
+  }
+
+function fuzzyMatch(drivers, firstTwoLetters) {
+    return drivers.filter((el) => el.slice(0, 2)===firstTwoLetters);
+}
+
+function matchName(drivers, name) {
+    console.log(drivers);
+    console.log(name);
+    return drivers.filter((el) => el.name.includes(name));
+}
